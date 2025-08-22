00f69b9437e9b48713c9e3e1d5bc73695916aeb3
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-08-22 17:49:29 +0200

    behaviorSubject implementierung vorgenomen

--- src/app/data-cache.ts
+++ src/app/data-cache.ts
@@ -1,19 +1,31 @@
 import { Injectable, signal } from '@angular/core';
 import { Person } from './person';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataCache {
-  private _data = signal<Person[]>([]);
-  readonly data = this._data.asReadonly();
 
   constructor() { 
     console.log('DataCache Service created!');
   }
+  
+  // Signal Ansatz
+  private _data = signal<Person[]>([]);
+  readonly data = this._data.asReadonly();
 
   addPerson(person: Person) {
     this._data.update(current => [...current, person]);
   }
   
+  // BehaviorSubject Ansatz
+  private _data$ = new BehaviorSubject<Person[]>([]);
+  readonly data$ = this._data$.asObservable();
+
+  addPersonObservable(person: Person) {
+    const current = this._data$.value;
+    this._data$.next([...current, person]);
+  }
+
 }
