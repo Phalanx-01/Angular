9fe04eed230c689aef52cf1978310902447557a5
Koutsouridis Loukas <phalanx.softdev@gmail.com>
2025-08-22 12:34:11 +0200

    json file and app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MyButton } from './shared/my-button/my-button';
+import { Active } from './shared/active';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MyButton, Active],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-app-1';
+  protected saved = true;
+  protected hasError = false;
 }
